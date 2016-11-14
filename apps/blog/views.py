7803817c32415a190fb302f75a2638d394961662
Kukrim Choe <choeku86@gmail.com>
2016-11-14 15:03:14 -0800

    Initial commit

---
+++ apps/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Blog
+
+# Create your views here.
+def index(request):
+  context = {
+    "blog": Blog.objects.order_by('-created_at'),
+  }
+
+  return render(request, "blog/index.html", context)
+
+
+def process(request):
+    Blog.objects.create(name=request.POST['name'], blog=request.POST['blog'])
+    return redirect('/')
+
